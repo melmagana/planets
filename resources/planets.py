1da198383713fb2d8465e2904df4c8b62087cc30
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-24 19:36:15 -0500

    dummy create route

--- resources/planets.py
+++ resources/planets.py
@@ -4,6 +4,10 @@ from flask import Blueprint
 
 planets = Blueprint('planets', 'planets')
 
-@planets.route('/')
+@planets.route('/', methods=['GET'])
 def planets_index():
 	return 'planets resource working'
+
+@planets.route('/', methods=['POST'])
+def create_planet():
+	return 'CREATE ROUTE for PLANET running'
