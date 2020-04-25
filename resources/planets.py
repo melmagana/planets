711ea1d8832e4b19c1f7381965f4408e8656d6f5
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-24 20:11:30 -0500

    Sends an API

--- resources/planets.py
+++ resources/planets.py
@@ -1,6 +1,7 @@
 import models
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from playhouse.shortcuts import model_to_dict
 
 planets = Blueprint('planets', 'planets')
 
@@ -13,5 +14,15 @@ def create_planet():
 	payload = request.get_json()
 	print(payload)
 	add_planet = models.Planet.create(name=payload['name'], planet_type=payload['planet_type'], length_of_year=payload['length_of_year'], moons=payload['moons'])
+	print('- ' * 20)
+	print('printed add_planet')
 	print(add_planet)
-	return 'CREATE ROUTE for PLANET running'
+	print('- ' * 20)
+	print('printed add_planet.__dict__')
+	print(add_planet.__dict__)
+	print('- ' * 20)
+	print('printed dir(add_planet)')
+	print(dir(add_planet))
+	# return 'CREATE ROUTE for PLANET running'
+	planet_dict = model_to_dict(add_planet)
+	return jsonify(data=planet_dict, message='Successfully created a planet!', status=201), 201
