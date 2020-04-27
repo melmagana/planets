e102f23c1fd1164f5091be47cfbb86915994bf49
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-27 17:49:42 -0500

    flask-cors

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from resources.planets import planets
 
 import models
 
+from flask_cors import CORS
+
 DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
+
+CORS(planets, origins=['http://localhost:3000'], support_credentials=True)
 
 app.register_blueprint(planets, url_prefix='/api/v1/planets')
 
