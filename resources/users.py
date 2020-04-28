a1682f94f247b3e0ac42031ba43b9b01192fa481
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-28 18:42:22 -0500

    registration works

--- resources/users.py
+++ resources/users.py
@@ -1,5 +1,7 @@
 import models
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash
+from playhouse.shortcuts import model_to_dict
 
 users = Blueprint('users', 'users')
 
@@ -10,5 +12,50 @@ def test_user():
 
 @users.route('/register', methods=['POST'])
 def register():
-	print(request.get_json())
-	return 'check terminal'
+	payload = request.get_json()
+
+	# make email and username case insensitive
+	payload['email'] = payload['email'].lower()
+	payload['username'] = payload['username'].lower()
+	print(payload)
+
+	# logic to see if user exists
+	try:
+		models.User.get(models.User.email == payload['email'])
+
+		# response
+		return jsonify(
+			data={},
+			message=f"A user with the email {payload['email']} already exists",
+			status=401
+		), 401
+
+	# logic if user does not exist
+	except models.DoesNotExist:
+
+		#scramble password with bcrypt
+		pw_hash = generate_password_hash(payload['password'])
+
+
+		#create user
+		create_user = models.User.create(
+			username=payload['username'],
+			email=payload['email'],
+			password=pw_hash
+		)
+
+		print(create_user)
+
+		create_user_dict = model_to_dict(create_user)
+		print(create_user_dict)
+
+		print(type(create_user_dict['password']))
+		# Shouldn't send encrypted password back, so get rid of it
+		create_user_dict.pop('password')
+
+
+		return jsonify(
+			data=create_user_dict,
+			message=f"Successfully registered user {create_user_dict['email']}",
+			status=201
+		), 201
