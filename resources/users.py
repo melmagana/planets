deb56e84af285b6036f0257104ca75c59591e06b
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-28 18:11:08 -0500

    can see JSON by sending it to POST /api/v1/users/register

--- resources/users.py
+++ resources/users.py
@@ -1,8 +1,14 @@
 import models
-from flask import Blueprint
+from flask import Blueprint, request
 
 users = Blueprint('users', 'users')
 
 @users.route('/', methods=['GET'])
 def test_user():
 	return 'user resource works'
+
+
+@users.route('/register', methods=['POST'])
+def register():
+	print(request.get_json())
+	return 'check terminal'
