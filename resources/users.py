7d31a555bf4b42a0475338a081688b2835212058
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-28 19:02:36 -0500

    login works

--- resources/users.py
+++ resources/users.py
@@ -1,7 +1,8 @@
 import models
 from flask import Blueprint, request, jsonify
-from flask_bcrypt import generate_password_hash
+from flask_bcrypt import generate_password_hash, check_password_hash
 from playhouse.shortcuts import model_to_dict
+from flask_login import login_user
 
 users = Blueprint('users', 'users')
 
@@ -46,6 +47,11 @@ def register():
 
 		print(create_user)
 
+
+		# logs in user and starts a session
+		login_user(create_user)
+
+
 		create_user_dict = model_to_dict(create_user)
 		print(create_user_dict)
 
@@ -59,3 +65,59 @@ def register():
 			message=f"Successfully registered user {create_user_dict['email']}",
 			status=201
 		), 201
+
+
+
+@users.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json()
+	payload['email'] = payload['email'].lower()
+	payload['username'] = payload['username'].lower()
+
+
+	# logic to look up user by email
+	try:
+		user = models.User.get(models.User.email == payload['email'])
+
+		# user with this email exists
+
+		user_dict = model_to_dict(user)
+
+		# check password using bcrypt
+		password_is_good = check_password_hash(user_dict['password'], payload['password'])
+
+		# logic if password is good
+		if(password_is_good):
+
+			# log the user in
+			login_user(user)
+
+			print(model_to_dict(user))
+
+			#response
+			return jsonify(
+				data=user_dict,
+				message=f"Successfully logged in {user_dict['email']}",
+				status=200
+			), 200
+
+
+		# logic if password is bad
+		else:
+			print('password is invalid')
+			return jsonify(
+				data={},
+				message="Email or password is incorrect",
+				status=401
+			), 401
+
+
+	# logic if they don't exist
+	except models.DoesNotExist:
+		print('username is invalid')
+
+		return jsonify(
+			data={},
+			message="Email or password is invalid",
+			status=401
+		), 401
